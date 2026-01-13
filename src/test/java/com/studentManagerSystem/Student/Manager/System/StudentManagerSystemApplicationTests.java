1eb0e854fb4774e0cb5fcdeb6b8671b3c1dfed55
AbdelhafidHassou <difa.hassou@gmail.com>
2026-01-13 18:20:43 +0100

    First commit - Setup project

---
+++ src/test/java/com/studentManagerSystem/Student/Manager/System/StudentManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentManagerSystem.Student.Manager.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
